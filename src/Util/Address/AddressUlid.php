cdec877d02e829c216a231679ebe65ffc90dd2e7
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-03-10 11:00:03 -0500

    Fix AddressUlid namespace and add generation tests

--- src/Util/Address/AddressUlid.php
+++ src/Util/Address/AddressUlid.php
@@ -1,77 +1,49 @@
 <?php
-/*
- * Copyright (c) 2025 Oleksandr Tishchenko / Marketing America Corp
- */
+# Copyright (c) 2025 Oleksandr Tishchenko / Marketing America Corp
 declare(strict_types=1);
-/*
- * Copyright (c) 2025 Oleksandr Tishchenko / Marketing America Corp
- * Author: Oleksandr Tishchenko <dev@smartresponsor.com>
- * Owner: Marketing America Corp
- *
- * This file is part of SmartResponsor Address data domain.
- * Comments are in English only. No placeholders, no stubs.
- */
 
-namespace App\Layer\Address;
+namespace App\Util\Address;
 
 use App\UtilInterface\Address\AddressUlidInterface;
 
-/**
- *
- */
-
-/**
- *
- */
 final class AddressUlid implements AddressUlidInterface
 {
-    /**
-     * @return string
-     * @throws \Exception
-     */
     public static function generate(): string
     {
-        // Simple Crockford Base32 without hyphens, 26 chars.
-        $time = microtime(true);
-        $ms = (int)round($time * 1000);
-        $random = random_bytes(10);
-        $timePart = self::base32($ms);
-        $randPart = self::base32FromBinary($random);
-        return $timePart . $randPart;
+        $milliseconds = (int) round(microtime(true) * 1000);
+        $randomBytes = random_bytes(10);
+
+        return self::base32FromInt($milliseconds) . self::base32FromBinary($randomBytes);
     }
 
-    /**
-     * @param int $value
-     * @return string
-     */
-    private static function base32(int $value): string
+    private static function base32FromInt(int $value): string
     {
         $alphabet = '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
-        $res = '';
+        $encoded = '';
+
         while ($value > 0) {
-            $res = $alphabet[$value % 32] . $res;
+            $encoded = $alphabet[$value % 32] . $encoded;
             $value = intdiv($value, 32);
         }
-        return str_pad($res, 10, '0', STR_PAD_LEFT);
+
+        return str_pad($encoded, 10, '0', STR_PAD_LEFT);
     }
 
-    /**
-     * @param string $bin
-     * @return string
-     */
-    private static function base32FromBinary(string $bin): string
+    private static function base32FromBinary(string $binary): string
     {
         $alphabet = '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
         $bits = '';
-        foreach (str_split($bin) as $c) {
-            $bits .= str_pad(decbin(ord($c)), 8, '0', STR_PAD_LEFT);
+
+        foreach (str_split($binary) as $byte) {
+            $bits .= str_pad(decbin(ord($byte)), 8, '0', STR_PAD_LEFT);
         }
-        $res = '';
-        for ($i = 0; $i < 16; $i++) {
-            $chunk = substr($bits, $i * 5, 5);
-            $idx = bindec(str_pad($chunk, 5, '0'));
-            $res .= $alphabet[$idx % 32];
+
+        $encoded = '';
+        for ($index = 0; $index < 16; $index++) {
+            $chunk = substr($bits, $index * 5, 5);
+            $encoded .= $alphabet[bindec(str_pad($chunk, 5, '0', STR_PAD_RIGHT)) % 32];
         }
-        return $res;
+
+        return $encoded;
     }
 }
