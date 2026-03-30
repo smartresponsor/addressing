8b45f893adc77810a7e5e79b8a2be44f2d94572b
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-03-30 00:18:19 -0500

    test: align qa loader with current runtime

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 // Managed by Commanding inspection
 
-use App\Kernel;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
 use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__.'/../vendor/autoload.php';
@@ -13,10 +14,10 @@ if (class_exists(Dotenv::class) && file_exists(__DIR__.'/../.env')) {
     (new Dotenv())->bootEnv(__DIR__.'/../.env');
 }
 
-$_SERVER['APP_ENV'] ??= 'dev';
-$_SERVER['APP_DEBUG'] ??= '1';
+$config = ORMSetup::createAttributeMetadataConfiguration([], true);
+$connectionParams = [
+    'driver' => 'pdo_sqlite',
+    'memory' => true,
+];
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$kernel->boot();
-
-return $kernel->getContainer()->get('doctrine')->getManager();
+return EntityManager::create($connectionParams, $config);
